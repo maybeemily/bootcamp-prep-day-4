e126101a84212018b5692942e7b7cf9833802d54
emily baier <emilyrbaier@gmail.com>
2019-01-17 19:23:48 -0800

    mapped out the greet function

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // eslint-disable-next-line
 function greet () {
-    console.log('greet function called');
+    // get the first name input by id
+    // get the last name input by id
+    // get values out of inputs
+    // make the string of the greeting 
+    // display the greeting
+    // remove prompt
+
 }
 
+//TODO: validate name inputs
+//TODO: clear inputs after greet? 
