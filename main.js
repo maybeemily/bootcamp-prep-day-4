f5cb703c53c8dd1aca01f31ae78760cf4edb72d5
emily baier <emilyrbaier@gmail.com>
2019-01-17 19:45:36 -0800

    added greeting

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 // eslint-disable-next-line
-function greet () {
-    // get the first name input by id
-    // get the last name input by id
+function greet() {
+    // get the first and last name input by id
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
+
     // get values out of inputs
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+
+    console.log(firstName, lastName);
+
     // make the string of the greeting 
+
+    var helloFullName = 'hello ' + firstName + ' ' + lastName + "!";
+    console.log(helloFullName);
+
     // display the greeting
+
     // remove prompt
 
 }
